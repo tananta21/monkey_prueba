61c297ddf65ef15e13ca0c3fd435709ef11bf1b2
tananta21 <kevintananta96@gmail.com>
2022-07-12 19:53:57 -0500

    updating

--- database/seeders/VehicleSeeder.php
+++ database/seeders/VehicleSeeder.php
@@ -16,27 +16,27 @@ class VehicleSeeder extends Seeder
     {
         DB::table('vehicles')->insert([
             'brand_id' => 1,
-            'name' => 'Mi vehiculo es',
+            'name' => 'Mi primer vehiculo',
             'number_plate' => 'MX-12321',
         ]);
 
-        DB::table('vehicles')->insert([
-            'brand_id' => 2,
-            'name' => 'Mi segundo vehiculo',
-            'number_plate' => 'VX-12321',
-        ]);
+        // DB::table('vehicles')->insert([
+        //     'brand_id' => 2,
+        //     'name' => 'Mi segundo vehiculo',
+        //     'number_plate' => 'VX-12321',
+        // ]);
 
-        DB::table('vehicles')->insert([
-            'brand_id' => 3,
-            'name' => 'Mi TERCER vehiculo',
-            'number_plate' => 'AO-745745',
-        ]);
+        // DB::table('vehicles')->insert([
+        //     'brand_id' => 3,
+        //     'name' => 'Mi TERCER vehiculo',
+        //     'number_plate' => 'AO-745745',
+        // ]);
 
-        DB::table('vehicles')->insert([
-            'brand_id' => 1,
-            'name' => 'Mi cuarto vehiculo',
-            'number_plate' => 'OP-745745',
-        ]);
+        // DB::table('vehicles')->insert([
+        //     'brand_id' => 1,
+        //     'name' => 'Mi cuarto vehiculo',
+        //     'number_plate' => 'OP-745745',
+        // ]);
         
     }
 }
